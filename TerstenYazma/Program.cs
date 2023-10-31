5a575c407b9f7e1c944b96aa433d77dd5c7460bc
Ezgi Adıgüzel <ezgiadiguzel01@gmail.com>
2023-10-31 22:28:00 +0300

    Add project files.

---
+++ TerstenYazma/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Lütfen bir metin giriniz");
+string metin = Console.ReadLine();
+//metin.Length "-1" deme sebebimiz diziler
+//diziler "0". indisden başlamaktır
+Console.WriteLine("Metnimizin ters hali");
+for (int i = metin.Length - 1; i >= 0; i--)
+{
+    Console.Write(metin[i]);
+}
